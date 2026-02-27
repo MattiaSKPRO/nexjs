4804c445d7c8ed3bcec597f08ed71da957461611
mattia.bassetto <m.bassetto@donboscosandona.it>
2026-02-27 10:07:28 +0100

    prosecco

--- prova/app/page.js
+++ prova/app/page.js
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
+    <>
     <h1>ciao tutti i cecchetto </h1>
+    <a href="/">Home page</a>
+    <br />
+    <a href="prodotti"> prodotti</a>
+    </>
   );
 }
--- prova/app/prodotti/page.js
+++ prova/app/prodotti/page.js
@@ -1,8 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function prodotti() {
   return (
+    <>
+    <div className="text-center">
  <h1>ciao tutti i cecchetto </h1>
+ <br />
+ 
+ <Link href={"/"} className="bg-blue-500 p-1 border-1 rounded-2xl text-center p-2">
+ Home page 
+ </Link>
+ </div>
+ </>
   );
 }
 
